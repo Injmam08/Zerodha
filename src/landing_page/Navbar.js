bbffdafc0bba1174e45e530a59d16fa2bd42d6be
injmam <injmamulhaque966@gmail.com>
2024-10-17 23:56:44 +0530

    home Component completed

--- src/landing_page/Navbar.js
+++ src/landing_page/Navbar.js
@@ -1,8 +1,45 @@
 import React from 'react'
 function NavBar() {
-    return ( 
-        <h1>NavBar</h1>
-     );
+  return (
+
+    <div className='container p-2 '>
+      <nav class="navbar navbar-expand-lg border-bottom "
+      style={{backgroundColor:"#FFF"}}>
+        <a class="navbar-brand" ><img style={{width:"30%"}} src='../media/images/logo.svg' alt='logo'></img> </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          
+          <form class="d-flex" role="search">
+       
+      </form>
+      <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link active" href="#">Signup<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="#"> About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="#">Product</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="#">pricing</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="#">Supports</a>
+            </li>
+            
+           
+          </ul>
+        </div>
+      </nav>
+
+    </div>
+
+  );
 }
 
 export default NavBar;
